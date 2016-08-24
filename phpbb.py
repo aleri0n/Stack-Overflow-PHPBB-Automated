26eb0bf24232739f2fa5d7dac4ea47cdd4839449
aleri0n <aleri0nv0rt3x@gmail.com>
2016-08-24 12:49:28 -0400

    Update phpbb.py

--- phpbb.py
+++ phpbb.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
-import requests, json
-
-# Stack-Overflow-PHPBB-Automated v1.0 by Aleri0n
-
+import requests 
+import json
 userinput = raw_input('Enter a keyword: ')
 userinputq = raw_input('Enter page: ')
-
+userinputr = raw_input('Enter secret: ')
+userinputs = raw_input('Full path to generator.php: ')
 getparams = {'page':userinputq, 'pagesize':'100', 'order':'desc', 'sort':'votes', 'intitle':userinput, 'site':'stackoverflow', 'filter': '!5-HwXhXgkSnzI0yfp0WqsC_-6BehEi(fRTZ7eg'}
-
 r = requests.get('https://api.stackexchange.com/2.2/search', params=getparams)
-
 result = json.loads(r.text)
 
 if result['has_more'] == False:
    print 'Error given.'
 else:
      for looping in result['items']:
-                  
          if looping['is_answered'] == True:
            try:
               newparams = {'order':'desc', 'sort':'votes', 'site':'stackoverflow', 'filter': '!4(Yrwr)RRK6oy2JSD'}
@@ -25,10 +21,7 @@ else:
               titletopost = 'Title:', looping['title']
               bodytopost = '<h1>Question:</h1><br>'+(looping['body'])+'<br>'+'Link to Question: '+(looping['link'])+'<br><br><br>'+'<h1>Answer:</h1><br>'+(newresult['items'][0]['body'])
               enterremove = bodytopost.replace('\n', '').replace('\r', '')
-              print enterremove
-              userdata = {"secret":"YOUR SECRET", "topic_title":titletopost, "body":enterremove}
-              requests.post("http://www.somewebsite.com/lib/generator.php", data=userdata)
-           except KeyError: print 'No answer ID found.'         
-         
-         print ''
-         print ''
+              userdata = {"secret":userinputr, "topic_title":titletopost, "body":enterremove}
+              requests.post(userinputs, data=userdata)
+              print 'Posted.'
+           except KeyError: print 'No answer ID found.'
